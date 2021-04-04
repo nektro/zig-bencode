975afc6a766746f2d890a783e8cf3b0e994a1a47
Meghan Denny <hello@nektro.net>
2021-04-04 05:18:25 -0700

    intial code commit

---
+++ src/lib.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+
+pub const Value = union(enum) {
+    String: []const u8,
+    Integer: usize,
+    List: []Value,
+    Dictionary: []std.StringArrayHashMap(Value).Entry,
+};
+
+fn peek(r: anytype) ?u8 {
+    const c = r.context;
+    if (c.pos == c.buffer.len) {
+        return null;
+    }
+    return c.buffer[c.pos];
+}
+
+const max_number_length = 25;
+
+/// Accepts a {std.io.FixedBufferStream} and a {std.mem.Allocator} to parse a Bencode stream.
+/// @see https://en.wikipedia.org/wiki/Bencode
+pub fn parse(r: anytype, alloc: *std.mem.Allocator) anyerror!Value {
+    const pc = peek(r) orelse return error.EndOfStream;
+    if (pc >= '0' and pc <= '9') return Value{ .String = try parseString(r, alloc), };
+
+    const t = try r.readByte();
+    if (t == 'i') return Value{ .Integer = try parseInteger(r, alloc), };
+    if (t == 'l') return Value{ .List = try parseList(r, alloc), };
+    if (t == 'd') return Value{ .Dictionary = try parseDict(r, alloc), };
+    return error.BencodeBadDelimiter;
+}
+
+fn parseString(r: anytype, alloc: *std.mem.Allocator) ![]const u8 {
+    const str = try r.readUntilDelimiterAlloc(alloc, ':', max_number_length);
+    const len = try std.fmt.parseInt(usize, str, 10);
+    var buf = try alloc.alloc(u8, len);
+    const l = try r.read(buf);
+    return buf[0..l];
+}
+
+fn parseInteger(r: anytype, alloc: *std.mem.Allocator) !usize {
+    const str = try r.readUntilDelimiterAlloc(alloc, 'e', max_number_length);
+    const x = try std.fmt.parseInt(usize, str, 10);
+    return x;
+}
+
+fn parseList(r: anytype, alloc: *std.mem.Allocator) ![]Value {
+    return error.Unreachable;
+}
+
+fn parseDict(r: anytype, alloc: *std.mem.Allocator) ![]std.StringArrayHashMap(Value).Entry {
+    var map = std.StringArrayHashMap(Value).init(alloc);
+    while (true) {
+        if (peek(r)) |c| {
+            if (c == 'e') {
+                return map.items();
+            }
+            const k = try parseString(r, alloc);
+            const v = try parse(r, alloc);
+            try map.put(k, v);
+        }
+        else {
+            break;
+        }
+    }
+    return error.EndOfStream;
+}
---
+++ zig.mod
@@ -0,0 +1,5 @@
+id: vwvoz97vjt4bjlyr6jdftzcuv6huc8ynjp4i7bjmqkdkt0hh
+name: bencode
+main: src/lib.zig
+license: MIT
+dependencies:
